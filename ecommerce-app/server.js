bbe8549101ca041760986baaecca857993532bfe
jerishimmanuel <jerishjimmanuel114@gmail.com>
2026-07-07 22:56:44 +0530

    2nd commit

---
+++ ecommerce-app/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on port ${process.env.PORT || 5000}`);
+});
